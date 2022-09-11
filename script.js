ae750da8624379fbaceaede1af12db473a179826
Taaaaab <owen.whitsett@gmail.com>
2022-09-11 14:29:04 -0400

    Draft of linked list factory function

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // Two classes or factories
 // factory function for linked list
 
-const createList = (nodeNumber) => {
+const createList = (fullList) => {
+    const append = (value) => createNode(value, end);
+    const prepend = (value) => createNode(value, start);
     return {
-        nodeNumber: nodeNumber
+        fullList: fullList
     }
 };
 
 const linkedList = createList(5);
-console.log(linkedList.nodeNumber)
+console.log(linkedList.fullList)
 
 // factory function for node
-const createNode = (nodeNumber) => {
+const createNode = (value, link) => {
     return {
-        nodeNumber: nodeNumber
+        value: null,
+        link: null,
     }
+
 };
 
-const node = createNode(1);
-console.log(node.nodeNumber)
+const node = createNode(1, null);
+console.log(node.value)
+
+// functions for linked lists
+function append(value) {
+    createNode(value)
+};
