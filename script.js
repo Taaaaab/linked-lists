3209611f7db5a1099944fa53c6e2920594725ccb
Taaaaab <owen.whitsett@gmail.com>
2022-09-11 20:27:33 -0400

    Add index search, size and to string linked list

--- script.js
+++ script.js
@@ -14,9 +14,34 @@ class linkedList {
     }
 
     append(data) {
+   
 
     }
-}
+
+    size() {
+        return this.length;
+    }
+
+    at(index) {
+        if (index < 0 || index >= this.length) return null
+
+        let current = this.head
+        for (let i = 0; i < index; i++) {
+            current = current.next
+        }
+        return current;
+    }
+
+    toString() {
+        let output = '';
+        let current = this.head;
+        while (current) {
+            output = `${output}${current.value} -> `
+            current = current.next;
+        }
+        console.log(`${output}null`);
+    }
+};
 
 class LinkedListNode {
     constructor(value, next) {
@@ -25,7 +50,15 @@ class LinkedListNode {
     }
 }
 
+
+
 const ll = new linkedList()
 ll.prepend(10);
 ll.prepend(20);
-console.log(ll);
+ll.prepend(30);
+ll.prepend(40);
+ll.append(5);
+// console.log(ll);
+console.log(ll.at(0));
+ll.toString();
+console.log(ll.size());
